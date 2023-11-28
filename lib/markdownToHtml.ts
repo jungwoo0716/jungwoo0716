263ef5f8992b7cb995206de15c29eaa7efb878e5
jungwoo0716 <kimjungwoo0716@gmail.com>
2023-11-28 20:45:34 +0900

    sanitize : false add

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -4,6 +4,6 @@ import toc from 'remark-toc'
 import gfm from 'remark-gfm'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).use(toc).use(gfm).process(markdown)
+  const result = await remark().use(html, { sanitize: false }).use(toc).use(gfm).process(markdown)
   return result.toString()
 }
