dc485f62e61772d9e47ce1daa5005f453f0d93d2
jungwoo0716 <kimjungwoo0716@gmail.com>
2023-11-28 17:01:25 +0900

    remark add

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,9 @@
 import { remark } from 'remark'
 import html from 'remark-html'
+import toc from 'remark-toc'
+import gfm from 'remark-gfm'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
+  const result = await remark().use(html).use(toc).use(gfm).process(markdown)
   return result.toString()
 }
