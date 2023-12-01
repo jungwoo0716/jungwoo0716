1f8bf7c33ee4bdcc6a307e73fd48189ca8db4e8f
jungwoo0716 <kimjungwoo0716@gmail.com>
2023-12-02 07:46:32 +0900

    sitemap add

--- next.config.js
+++ next.config.js
@@ -1,15 +1,31 @@
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const isProd = process.env.NODE_ENV === 'production';
+
+// const nextConfig = {
+//     basePath: isProd ? '/' : '',
+//     output: 'export',
+//     distDir: 'dist',
+//     images: {
+//         unoptimized: true,
+//     }
+//   };
+   
+//   module.exports = nextConfig
+
 /**
  * @type {import('next').NextConfig}
  */
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-    basePath: isProd ? '/' : '',
+    basePath: isProd ? '' : '', // 루트 도메인에 호스팅하는 경우
     output: 'export',
     distDir: 'dist',
     images: {
         unoptimized: true,
     }
-  };
-   
-  module.exports = nextConfig
+};
+
+module.exports = nextConfig;
