28b46b63268d8eadfa9ec1bafb6831fb3762de5e
jungwoo0716 <kimjungwoo0716@gmail.com>
2023-12-05 16:19:43 +0900

    mjm and polyjet 3d printing tech

--- lib/constants.ts
+++
@@ -1,4 +0,0 @@
-export const EXAMPLE_PATH = 'Always With Tech'
-export const CMS_NAME = 'Markdown'
-export const HOME_OG_IMAGE_URL =
-  'https://ai.esmplus.com/spacebar0702/og-image.jpg'
--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://jungwoo0716.github.io/',
-    generateRobotsTxt: true, // (optional)
-    // ...other options
-  }
