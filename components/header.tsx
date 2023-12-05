30aabdca2a86f88f394eb62dd9973d78832ae962
jungwoo0716 <kimjungwoo0716@gmail.com>
2023-12-05 16:20:02 +0900

    polyjet

---
+++ components/header.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import Script from 'next/script';
+
+const Header = () => {
+  <Script
+    async
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1067668052326016"
+    crossOrigin="anonymous"
+  />
+
+  return (    
+    <div>
+    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+      <Link href="/" className="hover:underline">
+        Always With Tech
+      </Link>
+      .
+    </h2>
+    <header>
+        <meta name="google-site-verification" content="gIiZlPtvReoFUr7eNqK3sIygXiwZCEPW83Wz1wtk604" />        
+        <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1067668052326016"
+        crossOrigin="anonymous"
+      />
+    </header>
+    </div>
+    
+  )
+}
+
+export default Header
---
+++ lib/constants.ts
@@ -0,0 +1,4 @@
+export const EXAMPLE_PATH = 'Always With Tech'
+export const CMS_NAME = 'Markdown'
+export const HOME_OG_IMAGE_URL =
+  'https://ai.esmplus.com/spacebar0702/og-image.jpg'
